ac0df3c5529215635bc7c5c50bfd3013e83ae76d
damon bakker <damon.bakker@hotmail.com>
2015-06-08 13:32:09 +0200

    misconception of syntax boys

--- app/database/migrations/2015_06_04_075249_build_database.php
+++ app/database/migrations/2015_06_04_075249_build_database.php
@@ -18,23 +18,23 @@ class BuildDatabase extends Migration {
 			$t->increments('user_id');
 			$t->integer('type_id');
 			$t->string('name',45);
-		})
+		});
 	
 		Schema::create('budget', function($t){
 			$t->integer('type_id')->primary();
 			$t->string('type',20);
 			$t->integer('budget');
-		})
+		});
 
 		Schema::create('user_department', function($t){
 			$t->integer('user_id')->primary();
 			$t->integer('department_code')->primary();
-		})
+		});
 
 		Schema::create('department', function($t){
 			$t->integer('department_code')->primary();
 			$t->string('department',30);
-		})
+		});
 		/*End user data*/
 
 		/*Start service data*/
@@ -48,12 +48,12 @@ class BuildDatabase extends Migration {
 			$t->integer('status');
 			$t->string('description',400);
 			$t->string('location',15);
-		})
+		});
 
 		Schema::create('service_category', function($t){
 			$t->integer('category_code')->primary();
 			$t->string('category',10);
-		})
+		});
 
 		/*End service data*/
 
@@ -67,7 +67,7 @@ class BuildDatabase extends Migration {
 			$t->string('prefered_days',14);
 			$t->integer('status');
 			$t->string('location',15);
-		})
+		});
 
 		Schema::create('order_article', function($t){
 			$t->integer('order_id')->primary();
@@ -75,14 +75,14 @@ class BuildDatabase extends Migration {
 			$t->integer('price_code')->primary();
 			$t->integer('quantity');
 			$t->integer('total_delivered');
-		})
+		});
 
 		Schema::create('order_answer', function($t){
 			$t->integer('order_id')->primary();
 			$t->integer('article_code')->primary();
 			$t->integer('question_id')->primary();
 			$t->string('answer',300);
-		})
+		});
 		/*End order data*/
 
 		/*Start article data*/
@@ -96,12 +96,12 @@ class BuildDatabase extends Migration {
 			$t->boolean('special');
 			$t->boolean('hidden');
 			$t->string('description',130);
-		})
+		});
 
 		Schema::create('article_category', function($t){
 			$t->integer('category_code')->primary();
 			$t->string('category',45);
-		})
+		});
 
 		Schema::create('article_data', function($t){
 			$t->integer('article_code')->primary();
@@ -109,7 +109,7 @@ class BuildDatabase extends Migration {
 			$t->integer('supply');
 			$t->decimal('price',6,2);
 			$t->string('location',45);
-		})
+		});
 
 		Schema::create('discarded', function($t){
 			$t->integer('article_code')->primary();
@@ -117,19 +117,19 @@ class BuildDatabase extends Migration {
 			$t->dateTime('date');
 			$t->string('reason',80);
 			$t->integer('quantity');
-		})
+		});
 
 		Schema::create('article_question', function($t){
 			$t->integer('article_code');
 			$t->increments('question_id');
 			$t->string('question',60);
-		})
+		});
 
 		Schema::create('question_answer', function($t){
 			$t->integer('question_id');
 			$t->increments('answer_id');
 			$t->string('answer',30);
-		})
+		});
 
 		/*End article data*/
 	

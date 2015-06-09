6b5bfa7ff2ff4b1c2162684c261ec9482b29a5f5
Frank Blom <frank@frankblom.nl>
2015-06-09 13:19:02 +0200

    fixed typos

--- app/routes.php
+++ app/routes.php
@@ -3,33 +3,34 @@
 Route::get('/', function(){
 	return View::make('hello');
 });
+
 Route::group(array('before'=>'guest'),function(){
-	Route::get('/login',array('as'=>'getLogin','uses'=>'UserController@getLogin'));
+	Route::get('/login', array('as'=>'getLogin','uses'=>'UserController@getLogin'));
 	Route::post('/login','UserController@postLogin');
 });
 
 Route::group(array('before'=>'auth'),function(){
 	Route::group(array('prefix'=>'artikelen'),function(){	
-		Route::get('/'array('as'=>'getArticles','uses'=>'ArticleController@index'));
-		Route::get('/{id}',array('as'=>'getArticle','uses'=>'ArticleController@show'));
-		Route::get('/{id}/edit',array('as'=>'editArticle','uses'=>'ArticleController@edit'));
+		Route::get('/', array('as'=>'getArticles','uses'=>'ArticleController@index'));
+		Route::get('/{id}', array('as'=>'getArticle','uses'=>'ArticleController@show'));
+		Route::get('/{id}/edit', array('as'=>'editArticle','uses'=>'ArticleController@edit'));
 
 		Route::post('/store','ArticleController@create');
 		Route::put('/{id}','ArticleController@update');
 		Route::delete('/{id}','ArticleController@delete');
 	});
 	Route::group(array('prefix'=>'diensten'),function(){
-		Route::get('/',array('as'=>'getServices','uses'=>'ServiceController@index'));
-		Route::get('/{id}'array('as'=>'getService','uses'=>'ServiceController@show'));
-		Route::get('/{id}/edit',array('as'=>'editService','uses'=>'ServiceController@edit'));
+		Route::get('/', array('as'=>'getServices','uses'=>'ServiceController@index'));
+		Route::get('/{id}', array('as'=>'getService','uses'=>'ServiceController@show'));
+		Route::get('/{id}/edit', array('as'=>'editService','uses'=>'ServiceController@edit'));
 
-		Route::post('/store','ServiceController@create');
+		Route::post('/store', 'ServiceController@create');
 		Route::put('/{id}','ServiceController@update');
 		Route::delete('/{id}','ServiceController@delete');
 	});
-	Route::get('/leveringen',array('as'=>'getDelever','uses'=>'DeliverController@index'));
-	Route::get('/aanvragen',array('as'=>'getMyRequests','uses'=>'RequestController@index'));
+	Route::get('/leveringen', array('as'=>'getDelever','uses'=>'DeliverController@index'));
+	Route::get('/aanvragen', array('as'=>'getMyRequests','uses'=>'RequestController@index'));
 	Route::get('/bestellingen', array('as'=>'getMyOrders','uses'=>'OrderController@index'));
-	Route::get('/statistieken',array('as'=>'getStatistics','uses'=>'StatisticsController@index'));
-	Route::get('/logout',array('as'=>'getLogout','uses'=>'UserController@getLogout'));
+	Route::get('/statistieken', array('as'=>'getStatistics','uses'=>'StatisticsController@index'));
+	Route::get('/logout', array('as'=>'getLogout','uses'=>'UserController@getLogout'));
 });

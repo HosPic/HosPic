1310e5c0252783fe69f4749a6a2248f9592ae6f5
MarkV28 <mark_vels@hotmail.com>
2015-06-09 16:10:20 +0200

    Replace url Add searchbar

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -5,9 +5,11 @@
 	</head>
 	<body>
 		@include('partials.menu')
+		{{-- @include('partials.search') --}}
 		<div></div>
 		{{ HTML::script("js/vendor/jquery.js") }}
 		{{ HTML::script("js/foundation.min.js") }}
+		{{ HTML::script("js/foundation/foundation.topbar.js") }}
 		@yield('script')
 	</body>
 </html>
--- app/views/partials/menu.blade.php
+++ app/views/partials/menu.blade.php
@@ -1,28 +1,28 @@
 <nav class="top-bar primary_color" data-topbar role="navigation">
 	<ul class="title-area secondary_color">
 		<li class="name">
-			<h1><a href='#'>HosPic</a></h1>
+			<h1><a href="{{ URL::to('/'); }}">HosPic</a></h1>
 		</li>
 		<li class="toggle-topbar menu-icon">
-			<a href="#"><span>Menu</span></a>
+			<a href=""><span>Menu</span></a>
 		</li>
 	</ul>
 	<section class="top-bar-section">
 		<ul class="right">
-			<li><a href="#">Uitloggen</a></li>
+			<li><a href="{{ URL::route('getLogout'); }}">Uitloggen</a></li>
 		</ul>
 		<ul class="left">
-			<li class="active"><a href="#">Artikelen bestellen</a></li>
-			<li><a href="#">Speciale bestelling</a></li>
-			<li><a href="#">Dienst aanvragen</a></li>
-			<li><a href="#">Mijn aanvragen</a></li>
-			<li><a href="#">Mijn bestellingen</a></li>
+			<li class="active"><a href="{{ URL::route('getOrder'); }}">Artikelen bestellen</a></li>
+			<li><a href="{{ URL::route('getOrder'); }}">Speciale bestelling</a></li>
+			<li><a href="{{ URL::route('getService'); }}">Dienst aanvragen</a></li>
+			<li><a href="{{ URL::route('getDeliver'); }}">Mijn aanvragen</a></li>
+			<li><a href="{{ URL::route('getMyOrders'); }}">Mijn bestellingen</a></li>
 			{{-- @if (User::canView("voorraad beheer")) --}}
-				<li><a href="#">Voorraad beheer</a></li>
+				<li><a href="{{ URL::route('getArticles'); }}">Voorraad beheer</a></li>
 			{{-- @elseif (User::canView("bezorglijst")) --}}
-				<li><a href="#">Bezorglijst</a></li>
+				<li><a href="{{ URL::route('getDeliver'); }}">Bezorglijst</a></li>
 			{{-- @elseif (User::canView("statistieken")) --}}
-				<li><a href="#">Statistieken</a></li>
+				<li><a href="{{ URL::route('getStatistics'); }}">Statistieken</a></li>
 			{{-- @endif --}}
 		</ul>
 	</section>

6f4d9289fb6cfffe20991ee3379d5d3d54f5e365
Frank Blom <frank@frankblom.nl>
2015-06-10 14:30:28 +0200

    lowercase

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -28,8 +28,8 @@ $env = $app->detectEnvironment(array(
 	'frank' => array('Axiom.local'),
 	'damon' => array('normandy-SR3'),
 	'mark' => array('Laptopper-PC', 'Mighty-1'),
-	'staging' => array('NMC-OS'),
-	'production' => array('NMC-UO'),
+	'staging' => array('nmc-uo'),
+	'production' => array('NMC-OS'),
 ));
 
 /*

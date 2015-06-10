beb479f774ef0bac2a615eb95ab1b6fe8f20123f
MarkV28 <mark_vels@hotmail.com>
2015-06-10 16:18:20 +0200

    Switch lowercase uppercase

--- app/views/partials/menu.blade.php
+++ app/views/partials/menu.blade.php
@@ -16,11 +16,11 @@
 			<li><a href="{{ URL::route('getService'); }}">Dienst aanvragen</a></li>
 			<li><a href="{{ URL::route('getDeliver'); }}">Mijn aanvragen</a></li>
 			<li><a href="{{ URL::route('getMyOrders'); }}">Mijn bestellingen</a></li>
-			{{ @if (auth::User->canView("voorraad beheer")) }}
+			{{ @if (Auth::user->canView("voorraad beheer")) }}
 				<li><a href="{{ URL::route('getArticles'); }}">Voorraad beheer</a></li>
-			{{ @elseif (auth::User->canView("bezorglijst")) }}
+			{{ @elseif (Auth::user->canView("bezorglijst")) }}
 				<li><a href="{{ URL::route('getDeliver'); }}">Bezorglijst</a></li>
-			{{ @elseif (auth::User->canView("statistieken")) }}
+			{{ @elseif (Auth::user->canView("statistieken")) }}
 				<li><a href="{{ URL::route('getStatistics'); }}">Statistieken</a></li>
 			{{ @endif }}
 		</ul>

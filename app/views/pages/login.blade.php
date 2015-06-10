ee31e323566a0074aebb606236dc64be0e2dc8bd
Frank Blom <frank@frankblom.nl>
2015-06-10 14:09:41 +0200

    removed bootstrap link

--- app/views/pages/login.blade.php
+++ app/views/pages/login.blade.php
@@ -1,8 +1,7 @@
 <!doctype html>
 <html>
 	<head>
-		{{-- @include('partials.head') --}}
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		@include('partials.head')
 		{{ HTML::style("css/login.css") }}
 	</head>
 <body>
